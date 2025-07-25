3ac72d395fef55c964e1cac29c317f725d95397f
sagevik <rsagevik@gmail.com>
2025-07-25 13:00:18 +0200

    add nmtui and sort network list

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -8,6 +8,8 @@ open_lock=''
 wifi_enable='󰖩'
 wifi_disable='󰖪'
 
+nmtui="NetworkManager TUI"
+
 notify-send "Getting list of available Wi-Fi networks..."
 wifi_list=$(nmcli --fields "SECURITY,SSID" device wifi list | sed 1d | sed 's/  */ /g' | sed -E "s/WPA*.?\S/$shut_lock /g" | sed "s/^--/$open_lock /g" | sed "s/$shut_lock  $shut_lock/$shut_lock/g" | sed "/--/d")
 
@@ -18,7 +20,9 @@ elif [[ "$connected" =~ "disabled" ]]; then
 	toggle="$wifi_enable  Enable Wi-Fi"
 fi
 
-selected_network=$(echo -e "$toggle\n$wifi_list" | sort | uniq | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: " -theme ${config_dir}/wi-fi )
+sorted_networks=$(echo -e "$toggle\n$wifi_list" | sort | uniq )
+selected_network=$(echo -e "$sorted_networks\n$nmtui" | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: " -theme ${config_dir}/wi-fi )
+# selected_network=$(echo -e "$toggle\n$launch_nmtui\n$wifi_list" | sort | uniq | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: " -theme ${config_dir}/wi-fi )
 # selected_network=$(echo -e "$toggle\n$wifi_list" | uniq -u | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: " -theme ${config_dir}/wi-fi )
 read -r chosen_id <<< "${selected_network:3}"
 
@@ -28,6 +32,8 @@ elif [ "$selected_network" = "$wifi_enable  Enable Wi-Fi" ]; then
 	nmcli radio wifi on
 elif [ "$selected_network" = "$wifi_disable  Disable Wi-Fi" ]; then
 	nmcli radio wifi off
+elif [ "$selected_network" = "$nmtui" ]; then
+	ghostty -e nmtui
 else
   success_message="You are now connected to the Wi-Fi network \"$chosen_id\"."
 	saved_connections=$(nmcli -g NAME connection)
